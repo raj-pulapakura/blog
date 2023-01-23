14b715ca4dd2d0adcd84dd51c8180c38e31a558f
Raj Pulapakura <raj.pulapakura@gmail.com>
2023-01-23 19:52:42 +1100

    fixed BlogCard component

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -12,7 +12,7 @@ export default function BlogCard({title, tags, coverPhoto, createdAt, updatedAt,
                     
                     <p className="text-base text-gray-500 mb-2">Last updated: {updatedAt.slice(0,10)}</p>
                     
-                    <p className="text-gray-500 mb-5">{content.text.slice(0, 200) + "..."}</p>
+                    <p className="text-gray-500 mb-5">{content.text.slice(0, 200).replace("//n", " ") + "..."}</p>
                     <div className="flex gap-x-4">
                         {tags.map(tag => <p className=" bg-gray-100 p-1 rounded-md">{tag}</p>)}
                     </div>
